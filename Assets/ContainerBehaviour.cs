bf9480bd49f91e93124b063365b19960558758d6
Alan N. Achaval <achavalalann@gmail.com>
2019-05-25 20:57:03 -0300

    Mantener los contenedor adyacentes luego de cambiar el volumen

--- Assets/ContainerBehaviour.cs
+++ Assets/ContainerBehaviour.cs
@@ -4,10 +4,12 @@ public class ContainerBehaviour : MonoBehaviour
 {
     public GameObject container = null;
     public const float relacionVolumenSize = 0.0447f;
+    public float relacionPositionSize = 5f;
 
     public void ActualizarVolumen(float volumen)
     {
         float size = relacionVolumenSize * volumen;
+        container.transform.localPosition = new Vector3(size * relacionPositionSize, 0, 0);
         container.transform.localScale = new Vector3(size, size, size);
     }
 }
