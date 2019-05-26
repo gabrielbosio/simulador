27f0acc0ef3b0ceb2c4ce74472c511d676c3642b
Alan N. Achaval <achavalalann@gmail.com>
2019-05-25 21:04:29 -0300

    Primera implementacion tabiques

--- Assets/ContainerBehaviour.cs
+++ Assets/ContainerBehaviour.cs
@@ -5,11 +5,17 @@ public class ContainerBehaviour : MonoBehaviour
     public GameObject container = null;
     public const float relacionVolumenSize = 0.0447f;
     public float relacionPositionSize = 5f;
+    public GameObject tabique = null;
 
     public void ActualizarVolumen(float volumen)
     {
         float size = relacionVolumenSize * volumen;
         container.transform.localPosition = new Vector3(size * relacionPositionSize, 0, 0);
         container.transform.localScale = new Vector3(size, size, size);
+    }
+
+    public void ActivarTabique(bool activado)
+    {
+        tabique.SetActive(activado);
     }
 }
---
+++ Assets/TabiquesBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TabiquesBehaviour : MonoBehaviour
+{
+    public ContainerBehaviour containerIzquierda;
+    public ContainerBehaviour containerDerecha;
+
+    public void ActivarTabique(bool activado)
+    {
+        containerIzquierda.ActivarTabique(activado);
+        containerDerecha.ActivarTabique(activado);
+    }
+}
